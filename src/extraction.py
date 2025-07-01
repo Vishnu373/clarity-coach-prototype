137bb885b7ae3de58f3bb1bf17f53c92e43e2cf7
Vishnu Vinay <vishnu1999w@gmail.com>
2025-07-01 11:29:45 -0400

    Digital pdf extraction code updated

--- src/extraction.py
+++ src/extraction.py
@@ -16,11 +16,35 @@ class DigitalPDFPipeline:
         clustered_lines = []
 
         with pdfplumber.open(self.filepath) as pdf:
-            for page in pdf.pages:
+            for page_index, page in enumerate(pdf.pages):
                 words = page.extract_words()
-                lines = defaultdict(list)
 
+                # Deleting the table extraction part
+                # 1. Getting the bounding boxes
+                table_bboxes = []
+                try:
+                    table_settings = {"vertical_strategy": "lines", "horizontal_strategy": "lines"}
+                    tables = page.find_tables(table_settings)
+                    table_bboxes = [table.bbox for table in tables]
+                except Exception as e:
+                    print(f"Warning: Could not find tables with pdfplumber on page {page_index + 1}: {e}")
+
+                # 2. Filter out words present from the table
+                filtered_words = []
                 for word in words:
+                    x0, y0, x1, y1 = word['x0'], word['top'], word['x1'], word['bottom']
+                    in_table = False
+                    for bbox in table_bboxes:
+                        x_min, y_min, x_max, y_max = bbox
+                        if x0 >= x_min and x1 <= x_max and y0 >= y_min and y1 <= y_max:
+                            in_table = True
+                            break
+                    if not in_table:
+                        filtered_words.append(word)
+
+                # 3. regular text extraction
+                lines = defaultdict(list)
+                for word in filtered_words:
                     y = round(word['top'] / self.y_tolerance) * self.y_tolerance
                     lines[y].append((word['x0'], word['text']))
 
@@ -30,6 +54,7 @@ class DigitalPDFPipeline:
 
         self.text_by_page = clustered_lines
         return self.text_by_page
+
 
     def extract_tables(self):
         try:
